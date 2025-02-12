635f5aea6f4c6bb7f3747c93efe27ca1c69524d0
SasakaRoben <sasakarobenld@gmail.com>
2025-02-12 06:02:19 +0300

    Create the operate function

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ function multiply(a, b){
 function divide(a, b){
     return a / b;
 }
+
+let number1;
+let number2;
+let operation;
+
+function operate(operation, number1, number2){
+    switch(operation){
+        case '+':
+            return add(number1, number2);
+        case '-':
+            return subtract(number1, number2);
+        case '*':
+            return multiply(number1, number2);
+        case '/':
+            if(number2 != 0){
+                return divide(number1, number2);
+            }
+            else{
+                return "Error: Division by zero is not allowed";
+            }
+    }
+}
+
